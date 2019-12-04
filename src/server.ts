70df4627beb90a1091d63b912bb55736740397d7
Flavio Miranda <vinniimiranda@gmail.com>
2019-12-04 14:39:12 -0300

    exceptionHandler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import './bootstrap';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 import routes from './routes';
 import './database';
 
@@ -10,6 +12,7 @@ class AppServer {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.excpetionHandling();
   }
 
   private routes() {
@@ -18,6 +21,13 @@ class AppServer {
 
   private middlewares() {
     this.server.use(express.json());
+    this.server.use(cookieParser());
+  }
+
+  excpetionHandling() {
+    this.server.use((err, req, res, next) => {
+      return res.status(err.status).json(err);
+    });
   }
 }
 export default new AppServer().server;
