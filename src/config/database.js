e02a23527cff0aef31a1a4283e24b0ccd034dc4f
Flavio Miranda <vinniimiranda@gmail.com>
2019-12-05 16:57:17 -0300

    Migrations

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+require('dotenv/config');
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+};
---
+++ src/database/migrations/20191205192240-users.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
